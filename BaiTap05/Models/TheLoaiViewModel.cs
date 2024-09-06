30162e64dd2ced432edc6dbd0e4aa0732b3c0621
GiaHan <ph.han3010@gmail.com>
2024-09-06 18:13:39 +0700

    Init project BT05

---
+++ BaiTap05/Models/TheLoaiViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Models
+{
+    public class TheLoaiViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
