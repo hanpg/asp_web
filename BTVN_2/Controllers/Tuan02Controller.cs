2c70bcc256e96268466cf9d9a280d1bc7fef0194
GiaHan <ph.han3010@gmail.com>
2024-09-09 12:10:56 +0700

    BaiTapVeNha02

---
+++ BTVN_2/Controllers/Tuan02Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN_2.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Phạm Gia Hân";
+            ViewBag.MSSV = "1822040666";
+            ViewData["Nam"] = "Năm 2024";
+            return View();
+        }
+    }
+}
+public class Tuan02Controller : Controller
+{
+
+    public IActionResult MayTinh(int a, int b, string pheptinh)
+    {
+        double PhepTinh;
+        if (pheptinh == "cong")
+            PhepTinh = a + b;
+        else if (pheptinh == "tru")
+            PhepTinh = a - b;
+        else if (pheptinh == "nhan")
+            PhepTinh = a * b;
+        else
+            PhepTinh = (double)a / b;
+        ViewBag.KetQua = PhepTinh;
+        return View();
+    }
+    public IActionResult Profile()
+    {
+        return View();
+    }
+}
--- BaiTap02/BaiTap02/obj/Debug/net6.0/BaiTap02.AssemblyInfo.cs
+++ BaiTap02/BaiTap02/obj/Debug/net6.0/BaiTap02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTap02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30162e64dd2ced432edc6dbd0e4aa0732b3c0621")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTap02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTap02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
